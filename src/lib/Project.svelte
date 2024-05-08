a2c1bc5c92e0e6c6ed4fe310f1df2b9556dfb714
Conor Egan <68134729+c-eg@users.noreply.github.com>
2024-05-08 22:08:14 +0100

    add projects page

---
+++ src/lib/Project.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let link;
+	export let title;
+	export let description;
+	export let stars;
+</script>
+
+<a href={link} target="_blank" class="flex flex-col gap-2 text-[var(--colour-primary)] justify-center items-start">
+	<p class="text-2xl text-[var(--colour-secondary)] font-medium">{title}</p>
+	<p class="text-xl font-normal">{description}</p>
+	<div class="flex gap-2">
+        <enhanced:img src="$lib/assets/star.svg" alt="Star" />
+        <p class="text-xl font-medium">{stars}</p>
+    </div>
+</a>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,4 +1,31 @@
+<script>
+	import Project from '$lib/Project.svelte';
+</script>
+
 <svelte:head>
 	<title>Projects | Conor Egan</title>
-	<meta name="description" content="Portfolio website for Conor Egan, a Software Engineer." />
+	<meta name="description" content="Conor Egan's software projects." />
 </svelte:head>
+
+<div class="flex flex-col justify-center items-center">
+	<h1
+		class="text-[2.5rem] leading-[3rem] text-[var(--colour-primary)] drop-shadow-2xl font-medium"
+	>
+		Here are some of my <span class="text-[var(--colour-secondary)]">projects</span>
+	</h1>
+	
+	<div class="flex flex-col gap-[2.5rem] mt-16 max-w-4xl w-full">
+		<Project
+			link={"https://github.com/c-eg/themoviedbapi"}
+			title={'TheMovieDB API'}
+			description={'A Java wrapper around the JSON API provided by TheMovieDB.org'}
+			stars={258}
+		/>
+		<Project
+			link={"https://github.com/c-eg/themoviedbapi"}
+			title={'Show Renamer'}
+			description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,"}
+			stars={258}
+		/>
+	</div>
+</div>
