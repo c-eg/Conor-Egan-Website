3bc82013178aa723904a4e7facaf453d940fed4c
Conor Egan <68134729+c-eg@users.noreply.github.com>
2024-05-08 22:09:30 +0100

    add alignment

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -8,7 +8,7 @@
 <a href={link} target="_blank" class="flex flex-col gap-2 text-[var(--colour-primary)] justify-center items-start">
 	<p class="text-2xl text-[var(--colour-secondary)] font-medium">{title}</p>
 	<p class="text-xl font-normal">{description}</p>
-	<div class="flex gap-2">
+	<div class="flex justify-center items-center gap-2">
         <enhanced:img src="$lib/assets/star.svg" alt="Star" />
         <p class="text-xl font-medium">{stars}</p>
     </div>
