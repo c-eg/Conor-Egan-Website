69aff96d9e7d69a02e9452f3794ec3733e336c89
Conor Egan <68134729+c-eg@users.noreply.github.com>
2024-05-22 22:01:02 +0100

    remove unused fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,8 @@
 <script>
 	import Nav from '$lib/Nav.svelte';
 	import '../app.css';
-	import '@fontsource/roboto/100.css';
-	import '@fontsource/roboto/300.css';
 	import '@fontsource/roboto/400.css';
 	import '@fontsource/roboto/500.css';
-	import '@fontsource/roboto/700.css';
-	import '@fontsource/roboto/900.css';
 </script>
 
 <div class="p-5">
