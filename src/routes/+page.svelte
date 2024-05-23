27ad1205b426510fee18b6d46b00e1d0a8f2f1b7
Conor Egan <68134729+c-eg@users.noreply.github.com>
2024-05-23 20:56:50 +0100

    update formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,22 +5,17 @@
 
 <div class="flex flex-col justify-center items-center gap-10 sm:gap-12 lg:gap-16">
 	<h1
-		class="text-5xl sm:text-7xl lg:text-8xl text-[var(--colour-primary)]
-		drop-shadow-2xl font-medium text-center"
+		class="text-5xl sm:text-7xl lg:text-8xl text-[var(--colour-primary)] drop-shadow-2xl font-medium text-center"
 	>
 		I'm <span class="text-[var(--colour-secondary)]">Conor Egan</span>
 	</h1>
 	<h2
-		class="text-2xl sm:text-4xl lg:text-5xl lg:leading-tight text-[var(--colour-primary)]
-		drop-shadow-2xl font-normal text-center max-w-sm sm:max-w-xl lg:max-w-3xl"
+		class="text-2xl sm:text-4xl lg:text-5xl lg:leading-tight text-[var(--colour-primary)] drop-shadow-2xl font-normal text-center max-w-sm sm:max-w-xl lg:max-w-3xl"
 	>
 		a Software Engineer, passionate about crafting digital solutions.
 	</h2>
 
-	<div
-		class="flex flex-col
-	sm:flex-row justify-center items-center gap-10 mt-8"
-	>
+	<div class="flex flex-col sm:flex-row justify-center items-center gap-10 mt-8">
 		<div class="border-[3px] border-[var(--colour-secondary)] rounded-full">
 			<enhanced:img
 				src="$lib/assets/profile-picture.jpg?w=250"
