55a282b014761aa9078391ca84d16bb98b86b1b8
Conor Egan <17conoregan@gmail.com>
2024-11-08 20:45:59 +0000

    change response to return null repositories

--- src/routes/projects/+page.server.js
+++ src/routes/projects/+page.server.js
@@ -10,7 +10,7 @@ export async function load() {
 	} catch (err) {
 		console.log(err);
 		return {
-			error: true
+			repositories: null
 		};
 	}
 }
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -4,7 +4,7 @@
 
 	let { data } = $props();
 
-	/** @type {Repository[] | undefined} */
+	/** @type {Repository[] | null} */
 	let repositories = data.repositories;
 </script>
 
@@ -20,7 +20,11 @@
 		Here are some of my <span class="text-[var(--colour-secondary)]">projects</span>
 	</h1>
 
-	{#if repositories !== undefined && repositories.length > 0}
+	{#if repositories === null}
+		<p class="text-xl text-[var(--colour-primary)] drop-shadow-2xl font-normal mt-20">
+			There was an error with the GitHub API.
+		</p>
+	{:else if repositories.length > 0}
 		<div class="flex flex-col gap-6 mt-16 max-w-3xl lg:max-w-4xl w-full">
 			{#each repositories as repo}
 				<Project
@@ -34,7 +38,7 @@
 		</div>
 	{:else}
 		<p class="text-xl text-[var(--colour-primary)] drop-shadow-2xl font-normal mt-20">
-			There was an error with the GitHub API.
+			There are currently no projects.
 		</p>
 	{/if}
 </div>
