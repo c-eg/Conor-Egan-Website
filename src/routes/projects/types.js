d2ab3d99fa3aa5495180681d0d33d631ff7ea4ad
Conor Egan <68134729+c-eg@users.noreply.github.com>
2024-12-07 23:18:08 +0000

    Project language selection (#55)
    * filtering repos functionality WIP
    
    * filtering
    
    * add sorting

---
+++ src/routes/projects/types.js
@@ -0,0 +1,5 @@
+/**
+ * @typedef {Object} SortOption
+ * @property {string} label - the title to display in the select element.
+ * @property {string} value - the value to be used for sorting logic.
+ */
